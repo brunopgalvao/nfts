66b473370976a14ed80a1dda1d191e3cd966de6f
Bruno Galvao <brunopgalvao@gmail.com>
2024-04-26 13:23:57 +0900

    refactor

--- lib.rs
+++ lib.rs
@@ -1,7 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
 use pop_api::nfts::*;
-use enumflags2::BitFlags;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode)]
 #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
@@ -17,35 +16,23 @@ impl From<Error> for ContractError {
 }
 
 #[ink::contract]
-mod my_nft {
+mod my_nfts {
     use super::*;
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
-    #[ink(storage)]
-    pub struct MyNft {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
-    }
+	#[ink(storage)]
+	#[derive(Default)]
+	pub struct MyNfts;
 
-    impl MyNft {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
-        #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
-        }
+    impl MyNfts {
+		#[ink(constructor, payable)]
+		pub fn new() -> Self {
+			ink::env::debug_println!("Nfts::new");
+			Default::default()
+		}
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        #[ink(message)]
+		#[ink(message)]
 		pub fn create_nft_collection( &self ) -> Result<(), ContractError>{
+			ink::env::debug_println!("Nfts::create_nft_collection: collection creation started.");
             let admin = Self::env().caller();
             let item_settings = ItemSettings(BitFlags::from(ItemSetting::Transferable));
 
@@ -63,9 +50,18 @@ mod my_nft {
                 mint_settings,
             };
             pop_api::nfts::create(admin, config)?;
-			// ink::env::debug_println!("Contract::create_nft_collection: collection created successfully");
-			// pop_api::nfts::mint(collection_id, item_id, receiver)?;
+			ink::env::debug_println!("Nfts::create_nft_collection: collection created successfully.");
             Ok(())
 		}
     }
+
+    #[cfg(test)]
+	mod tests {
+		use super::*;
+
+		#[ink::test]
+		fn default_works() {
+			MyNfts::new();
+		}
+	}
 }
